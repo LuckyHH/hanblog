f6dd04876c70965c9a59cdd4332bf81f93f3acdc
LuckyHH <1391380241@qq.com>
2018-02-08 15:27:30 +0800

    add rightNav

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    cookieSecret : 'hanblog',
-    db:'hanblog',
-    url:'mongodb://luckyhh:hj782161@ds259305.mlab.com:59305/hanblog'
+    cookieSecret : '<dbname>',
+    db:'<dbname>',
+    url:'mongodb://localhost:27017/<dbname>'
 };
